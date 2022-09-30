303eae9d5bc48827134b4d749c04bda3e19627c5
Iara Silvia Domiciano Crivelaro <66339183+IaraSilvia@users.noreply.github.com>
2022-09-30 10:30:25 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Shoes Lovers Store</h1>
+      <h2>The best shoes are here!</h2>
+    </div>
+  );
+}
